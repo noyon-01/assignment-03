57ad4c93ddacefe758a994eddbdcff1eca926c5a
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-07 00:02:40 +0600

    problem 4 is added in assignment 3

--- final-problem/final-problem.js
+++ final-problem/final-problem.js
@@ -46,3 +46,29 @@ function finalScore(omr) {
     return roundedTotalScore;
   }
 }
+
+// Problem - 04
+function gonoVote(array) {
+  const haVote = [];
+  const naVote = [];
+  if (!Array.isArray(array)) {
+    return "Invalid";
+  } else {
+    for (const arr of array) {
+      if (arr.includes("ha")) {
+        haVote.push(arr);
+      } else if (arr.includes("na")) {
+        naVote.push(arr);
+      }
+    }
+  }
+  const haVoteLength = haVote.length;
+  const naVoteLength = naVote.length;
+  if (haVoteLength > naVoteLength) {
+    return true;
+  } else if (haVoteLength === naVoteLength) {
+    return "equal";
+  } else {
+    return false;
+  }
+}
